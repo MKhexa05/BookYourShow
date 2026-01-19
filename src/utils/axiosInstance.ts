b5c2760f6046e64502445917a07b34af8ae90859
MK <mkroshnaiwala@hexacoder.com>
2026-01-19 12:42:32 +0530

    made changes in axiosInstance

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL: "/api",
+  withCredentials: true,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
