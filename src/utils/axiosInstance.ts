6bcf86782df5c7ac66b815bf16ef08a8faeeadb6
MK <mkroshnaiwala@hexacoder.com>
2026-01-19 12:36:33 +0530

    Added vercel.json

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://ec2-13-201-98-117.ap-south-1.compute.amazonaws.com:3000",
+  baseURL: "/api",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
